ce9385b20f3db29d9da33909501ab536f11a9f8c
Pete Hodgson <git@thepete.net>
2010-03-17 10:57:40 -0700

    Improved cell output spec

--- spec/cell_output_spec.rb
+++ spec/cell_output_spec.rb
@@ -1,34 +1,38 @@
 require File.dirname(__FILE__)+'/spec_helper.rb'
+require 'builder'
 
 require 'cell_output'
 
 describe CellOutput do
 
-  def make_stub_builder
-    stub_builder = stub('Builder')
+  before :each do
+    @cell_output = CellOutput.new
+    @builder = Builder::XmlMarkup.new
+  end
+
+  def xml_should_look_like(expected_output)
+    @builder.should == expected_output
   end
 
   it 'should render a vanilla cell correctly' do
-    cell_output = CellOutput.new
-    cell_output.content = 'cell contents'
-
-    stub_builder = make_stub_builder
-    stub_builder.should_receive( 'td' ).with( 'cell contents', {} )
-
-    cell_output.render_to( stub_builder )
+    @cell_output.content = 'cell contents'
+    xml_should_look_like "<td>cell contents</td>"
   end
 
   it 'should add any custom attributes to the td node' do
-    cell_output = CellOutput.new
-    cell_output.content = 'cell contents'
-    cell_output.attributes['foo'] = 'bar'
+    @cell_output.content = 'cell contents'
+    @cell_output.attributes['foo'] = 'bar'
+    xml_should_look_like %q|<td foo="bar">cell contents</td>|
+  end
 
-    stub_builder = make_stub_builder
-    stub_builder.
-      should_receive( 'td' ).
-      with( 'cell contents', 'foo' => 'bar' )
+  it 'should escape non-raw content' do
+    @cell_output.content = "X > Y"
+    xml_should_look_like %q|<td>X &gt; Y</td>|
+  end
 
-    cell_output.render_to( stub_builder )
+  it 'should not escape raw content' do
+    @cell_output.raw_content = "<i>foo</i>"
+    xml_should_look_like %q|<td><i>foo</i></td>|
   end
 
 end
