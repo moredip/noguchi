c86e3a8a71417b5f672ae75c44bc1d74a60b22fe
Pete Hodgson <git@thepete.net>
2010-03-23 16:10:36 -0700

    WIP on extracting a Field class out from Table

--- lib/table.rb
+++ lib/table.rb
@@ -10,9 +10,10 @@ class Table
 
   def initialize
     @fields = []
+    @field_ordering = []
+    @fields_hash = {}
     @columns = {}
     @data = []
-    @header_renderers = {}
     @custom_body_renderers = {}
 
     to_get_field_from_datum do |datum,field|
@@ -27,6 +28,8 @@ class Table
   def columns=(columns)
     @columns = {}
     @fields = []
+    @field_ordering = []
+    @fields_hash = {}
 
     header_labels, fields = break_columns_into_header_and_field_names(columns)
     fields.zip(header_labels) do |field,label|
@@ -35,27 +38,18 @@ class Table
   end
 
   def add_field(field,column_options = {})
-    unless column_options.has_key?(:label)
-      column_options[:label] = default_label_for(field)
-    end
+    register_field( Field.from_column_options( field, column_options ) )
 
     @fields << field 
     @columns[field] = column_options
-
-    to_render_header_cell_for(field) do |field,column_label,cell|
-      cell.content = column_label
-      if column_options.has_key?(:class)
-        cell.attributes[:class] = column_options[:class] 
-      end
-    end
   end
 
-  def to_get_field_from_datum(&field_extraction_proc)
-    @field_extraction_proc = field_extraction_proc
+  def to_get_field_from_datum(&field_value_extraction_proc)
+    @field_value_extraction_proc = field_value_extraction_proc
   end
 
   def to_render_header_cell_for( field, &proc )
-    @header_renderers[field] = proc
+    @fields_hash[field].custom_header_cell_render_proc = proc
   end
   
   def to_render_body_cell_for( *fields, &proc )
@@ -95,14 +89,15 @@ class Table
   end
 
   private
+
+  def register_field( field )
+    @field_ordering << field.name
+    @fields_hash[field.name] = field 
+  end
   
   def render_header_row( field, rendering_context )
     cell_output = CellOutput.new
-    header_renderer_for(field).call( 
-      field,
-      column_label_for(field),
-      cell_output
-    )
+    @fields_hash[field].render_header_cell(cell_output)
 
     rendering_context.render_raw_cell( 
       cell_output.raw_content,
@@ -110,20 +105,12 @@ class Table
     )
   end
 
-  def header_renderer_for(field)
-    @header_renderers[field]
-  end
-
-  def column_label_for(field)
-    @columns[field][:label]
-  end
-
   def render_body_cell( field, rendering_context )
     if @custom_body_renderers.has_key?(field)
       context = BodyCellRenderContext.new( 
         rendering_context.datum, 
         field, 
-        @field_extraction_proc 
+        @field_value_extraction_proc 
       )
       cell_output = CellOutput.new
       @custom_body_renderers[field].call( context, cell_output )
@@ -132,19 +119,11 @@ class Table
         cell_output.attributes
       )
     else
-      field_value = @field_extraction_proc.call(rendering_context.datum,field) 
+      field_value = @field_value_extraction_proc.call(rendering_context.datum,field) 
       rendering_context.render_cell( field_value )
     end
   end
 
-  def default_label_for(field)
-    if defined?(ActiveSupport::Inflector)
-      ActiveSupport::Inflector.titleize(field)
-    else
-      field.to_s
-    end
-  end
-  
   def break_columns_into_header_and_field_names(columns)
     header_names = []
     field_names = []
@@ -153,6 +132,34 @@ class Table
       field_names << field
     end
     [header_names,field_names]
+  end
+end
+
+class Field < Struct.new( :name, :column_label, :column_class )
+
+  attr_accessor :custom_header_cell_render_proc, :custom_body_cell_render_proc
+
+  def self.from_column_options( name, column_options )
+    column_label = column_options[:label] || default_label_for(name)
+    column_class = column_options[:class]
+    new( name, column_label, column_class )
+  end
+
+  def self.default_label_for(field_name)
+    if defined?(ActiveSupport::Inflector)
+      ActiveSupport::Inflector.titleize(field_name)
+    else
+      field_name.to_s
+    end
+  end
+
+  def render_header_cell( cell )
+    if @custom_header_cell_render_proc
+      @custom_header_cell_render_proc.call( name, column_label, cell )
+    else
+      cell.content = column_label
+      cell.attributes[:class] = column_class if column_class
+    end
   end
 end
 end
